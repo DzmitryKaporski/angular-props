6e8a28a1667e1f62a00d44fd874d65e83469d602
Dim@ <dzmitry.kaporski@gmail.com>
2022-06-21 08:28:11 +0300

    init: start

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    UserDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/user-details/user-details.component.ts
@@ -0,0 +1,24 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
+})
+export class UserDetailsComponent implements OnInit {
+
+  userLastName: string = 'Kaporski'
+  userFirstName: string = 'Dima'
+  userAge: number = 25
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get userFullName(): string {
+    return `${this.userFirstName} ${this.userLastName}`
+  }
+
+}
